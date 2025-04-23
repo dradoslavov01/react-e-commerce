b66c74e40427bf53aa5a932dbe17318fe474e5a6
yordan.yordanov <yordan.yordanov@diva-e.com>
2025-04-23 20:45:36 +0300

    updated styles to modules

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import './NavigationBar.scss'
+import styles from './NavigationBar.module.scss'
 
 const NavigationBar = (props) => {
   return (
-    <nav className="navbar">
+    <nav className={styles.navbar}>
       <h1>eCommerce</h1>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li className="dropdown">
+        <li className={styles.dropdown}>
           <a>Category</a>
-          <div className="dropdown_content">
+          <div className={styles.dropdown_content}>
             <Link to="/category/clothes">Clothes</Link>
             <Link to="/category/shoes">Shoes</Link>
             <Link to="/category/hats">Hats</Link>
@@ -24,30 +24,30 @@ const NavigationBar = (props) => {
         </li>
         {props.isLoggedIn
           ? (<>
-            <li className="cart_container">
+            <li className={styles.cart_container}>
               {props.cart.length > 0
                 ? (
-                  <span className="cart_counter">{props.cart.length}</span>
+                  <span className={styles.cart_counter}>{props.cart.length}</span>
                 )
                 : ''
               }
-              <Link to="/cart" className="user">Cart</Link>
+              <Link to="/cart" className={styles.user}>Cart</Link>
             </li>
-            <li className="profile_container">
+            <li className={styles.profile_container}>
               {props.favoriteItems.length > 0
                 ? (
-                  <span className="favItems_counter">{props.favoriteItems.length}</span>
+                  <span className={styles.favItems_counter}>{props.favoriteItems.length}</span>
                 )
                 : ''
               }
-              <Link to="/profile" className="user">Profile</Link>
+              <Link to="/profile" className={styles.user}>Profile</Link>
             </li>
             <li>
-              <Link to="/logout" className="user" onClick={() => { window.location.reload(); localStorage.clear() }}>Logout</Link>
+              <Link to="/logout" className={styles.user} onClick={() => { window.location.reload(); localStorage.clear() }}>Logout</Link>
             </li>
           </>
           )
-          : <li className="signIn"><Link to="/login">Sign in</Link></li>
+          : <li className={styles.signIn}><Link to="/login">Sign in</Link></li>
         }
       </ul>
     </nav>
